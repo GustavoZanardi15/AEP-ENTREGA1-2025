3ccf389f85ef9a99edacc430e77da401fa663151
GustavoZanardi15 <ulianzanardig@gmail.com>
2025-05-30 08:46:10 -0300

    Configuração do Tailwind CSS funcionando

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+import React from 'react';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-blue-100 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-blue-700">
+        Tailwind está funcionando!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
+
